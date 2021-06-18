2b13a247dbe0345c57580ce38bbce9aa2bf172ba
tkhwang <tkhwang@gmail.com>
2021-06-18 23:00:55 +0900

    ♻️ Add emoji server listening message.

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('🚀 Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
