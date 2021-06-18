9caa514203e58f28b27a7cdb95d7ba1b7ddc931a
tkhwang <tkhwang@gmail.com>
2021-06-19 07:15:40 +0900

    🚚  (backend): Move app module to the top

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,14 +5,13 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 5000;
   await app.listen(port, () => {
     Logger.log('🚀 Listening at http://localhost:' + port + '/' + globalPrefix);
   });
