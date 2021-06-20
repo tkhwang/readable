e0beaba2719e0ca85f8ea466ffd5f733ca9eb78d
Teddy <tkhwang@gmail.com>
2021-06-20 17:30:59 +0900

    ✨ (client): Add Apollo client configuration

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,10 +1,40 @@
-import styled from 'styled-components';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { GetUsers } from './components/GetUsers';
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      alert(`Graphql error ${message}`)
+    );
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: 'http://localhost:7000/graphql',
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
 
 export function App() {
-  return <>Readble: coming soon...</>;
+  return (
+    <ApolloProvider client={client}>
+      Readable, coming soon...
+      <GetUsers />
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ apps/client/src/app/components/Queries.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const LOAD_USERS = gql`
+  query {
+    userFindAll {
+      id
+      name
+    }
+  }
+`;
