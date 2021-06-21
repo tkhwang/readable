93b6da551deec5248e1dc918dfe8e4824a6136db
Teddy <tkhwang@gmail.com>
2021-06-21 21:54:32 +0900

    ✨ (client): Add codegen basic feature (#8)

---
+++ apps/client/src/app/components/useGetUsersViewModel.query.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+import { ViewModel } from '../../core/ViewModel';
+import { useUserFindAllQuery } from './useGetUsersViewModel.query.generated';
+
+const LOAD_USERS = gql`
+  query UserFindAll {
+    userFindAll: userFindAll {
+      id
+      name
+    }
+  }
+`;
+
+export type GetUsersViewModel = ViewModel<typeof useGetUsersViewModel>;
+
+export function useGetUsersViewModel() {
+  const { data, loading, error } = useUserFindAllQuery();
+
+  const users = data?.userFindAll ?? [];
+
+  return {
+    users,
+    loading,
+    error,
+  };
+}
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,9 +3,40 @@ import * as ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      alert(`Graphql error ${message}`)
+    );
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: 'http://localhost:7000/graphql',
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
   document.getElementById('root')
 );
