4ff9dd12cc8cd74d7ee652d220d3df529c4a6717
Teddy <tkhwang@gmail.com>
2021-06-22 00:25:27 +0900

    ✨ (server): MySQL database setup

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,19 +3,20 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'apps/server/src/graphql-types.gql'),
       sortSchema: true,
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   database: 'readable-dev',
-    //   entities: ['dist/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
+    // TODO(Teddy): TypeORM config
+    // TypeOrmModule.forRoot(),
     UsersModule,
   ],
   controllers: [],
