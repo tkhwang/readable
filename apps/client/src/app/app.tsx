2c71539b528f818d5701c4d1919dd39b7cb0d718
Teddy <tkhwang@gmail.com>
2021-06-22 22:16:43 +0900

    ✨ (client): Add symbol image

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -6,7 +6,7 @@ export function App() {
 
   return (
     <>
-      Readable, coming soon...
+      <img src="https://user-images.githubusercontent.com/365500/122930877-51c81b80-d3a7-11eb-8a7e-9b1de95cda77.png" alt="symbol" />
       <GetUsersViewController viewModel={viewModel} />
     </>
   );
