1e007a748c829e3179f10d8e5d0ac57062cca444
Teddy <tkhwang@gmail.com>
2021-06-27 02:28:52 +0900

    🔧 (backend): Setup GraphQL PORT number

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 80;
+  const port = process.env.PORT || 7000;
 
   await app.listen(port, () => {
     // Logger.log('🚀 Listening at http://localhost:' + port + '/' + globalPrefix);
