1f0c61a6f5a2afb18e99cc43f40e9e836ec6fd6b
Teddy <tkhwang@gmail.com>
2021-06-27 17:11:06 +0900

    🔧 (common): Check environment variable

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -8,14 +8,13 @@ export function App() {
   return (
     <>
       <div className="text-xl font-medium text-red-400">tailwind test!</div>
+      아래가 제대로 보여야 함 (GraphQL API 동작)
+      <GetUsersViewController viewModel={viewModel} />
       <img
         src="https://user-images.githubusercontent.com/365500/122947722-572c6280-d3b5-11eb-9b18-f8e0e2f79e3b.png"
         alt="symbol"
         style={{ width: '100%' }}
       />
-      아래가 제대로 보여야 함 (GraphQL API 동작)
-       <img src="https://user-images.githubusercontent.com/365500/122947722-572c6280-d3b5-11eb-9b18-f8e0e2f79e3b.png" alt="symbol" style={{ width: '100%'}} />
-      <GetUsersViewController viewModel={viewModel} />
     </>
   );
 }
--- apps/client/src/app/components/GetUsersViewController.tsx
+++ apps/client/src/app/components/GetUsersViewController.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { GetUsersViewModel } from './useGetUsersViewModel.query';
 import { ViewController } from '../../core/ViewController';
+import { graphQLServer } from 'src/link';
 
-export const GetUsersViewController: ViewController<GetUsersViewModel> = React.memo(
-  ({ viewModel }) => {
-    const { users, loading, error } = viewModel;
+export const GetUsersViewController: ViewController<GetUsersViewModel> = React.memo(({ viewModel }) => {
+  const { users, loading, error } = viewModel;
 
-    return (
+  const env = process.env.NODE_ENV;
+  const deployServer = process.env.DEPLOY_SERVER;
+
+  return (
+    <div className="m-5">
       <ul>
-        {users &&
-          users.map((user: any) => {
-            return (
-              <li key={user.id}>
-                {user?.id}:{user?.name}
-              </li>
-            );
-          })}
+        <li>NODE_ENV={env}</li>
+        <li>deployServer={deployServer}</li>
+        <li>graphQLServer={graphQLServer.url}</li>
+        <li>
+          GraphQL API result
+          {users &&
+            users.map((user: any) => {
+              return (
+                <li key={user.id}>
+                  {user?.id}:{user?.name}
+                </li>
+              );
+            })}
+        </li>
       </ul>
-    );
-  }
-);
+    </div>
+  );
+});
