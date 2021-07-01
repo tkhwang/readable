2746006b7933da346e3d66d7bdc8dac81640cb84
Teddy <tkhwang@gmail.com>
2021-07-02 06:42:15 +0900

    🛂 (server): Enable CORS in nest.js

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const globalPrefix = 'rest';
   app.setGlobalPrefix(globalPrefix);
