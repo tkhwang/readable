8a7a626cd4908c9d226bac98bb289aeecc7cc327
Teddy <tkhwang@gmail.com>
2021-07-04 09:36:16 +0900

    🛂 (server): Separate passportStrategies

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { FacebookStrategy } from './facebook.strategy';
-import { GithubStrategy } from './github.strategy';
-import { GoogleStrategy } from './google.strategy';
-import { TwitterStrategy } from './twitter.strategy';
+import { FacebookStrategy } from './passportStrategy/facebook.strategy';
+import { GithubStrategy } from './passportStrategy/github.strategy';
+import { GoogleStrategy } from './passportStrategy/google.strategy';
+import { TwitterStrategy } from './passportStrategy/twitter.strategy';
 
 @Module({
   imports: [UsersModule],
