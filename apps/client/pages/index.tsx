b62658e43e51e672c735159c9fdf4e63a5f839be
zlrlo <easy10612@gmail.com>
2021-07-04 11:47:10 +0900

    🔥 (client): Move code to login page

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import config from '../website-config';
 import AOS from 'aos';
+
 import { getDataFromTree } from '@apollo/client/react/ssr';
-import { GetUsersViewController } from '@readable/components/GetUser/GetUsersViewController';
 import withApollo from '@readable/lib/withApollo';
-import { SocialLoginButton } from '@readable/components/SocialLoginButton';
 import { useGetUsersViewModel } from '@readable/components/GetUser/useGetUsersViewModel.query';
+
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/Features';
@@ -34,31 +34,6 @@ export function Home() {
   });
 
   return (
-    // <>
-    //   <GetUsersViewController viewModel={viewModel} />
-    //   <div className="w-full p-4">
-    //     <main role="main" className="w-full flex flex-col h-screen content-center justify-center">
-    //       <div
-    //         onClick={() => {
-    //           console.log('hihihi');
-    //         }}
-    //         className="w-full sm:w-1/2 lg:w-1/3 bg-gray-50 rounded-xl m-auto"
-    //       >
-    //         <img
-    //           src="https://user-images.githubusercontent.com/365500/122947722-572c6280-d3b5-11eb-9b18-f8e0e2f79e3b.png"
-    //           alt="symbol"
-    //           style={{ width: '300 px' }}
-    //         />
-    //         <div className="bg-white rounded shadow px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-    //           <SocialLoginButton provider="google" />
-    //           <SocialLoginButton provider="facebook" />
-    //           <SocialLoginButton provider="github" />
-    //         </div>
-    //       </div>
-    //     </main>
-    //   </div>
-    // </>
-
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Head>
         <title>{config.title}</title>
