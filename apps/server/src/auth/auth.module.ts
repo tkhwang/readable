a0fd4a6fb1fed891694675c992afd2fe0cd23900
Teddy <tkhwang@gmail.com>
2021-07-06 00:10:43 +0900

    ♻️ (server): Change password strategy folder name  (#44)
    * 🛂 (server): Change passport strategy folder name
    
    * 📦 (server): Add npm package  - jsonwebtoken
    
    * 📦 (server): Add joi package
    
    * ♻️ (server): Auth passport strategy folder name changed

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { FacebookStrategy } from './passportStrategy/facebook.strategy';
-import { GithubStrategy } from './passportStrategy/github.strategy';
-import { GoogleStrategy } from './passportStrategy/google.strategy';
-import { TwitterStrategy } from './passportStrategy/twitter.strategy';
+import { FacebookStrategy } from './strategies/facebook.strategy';
+import { GithubStrategy } from './strategies/github.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { TwitterStrategy } from './strategies/twitter.strategy';
 
 @Module({
   imports: [UsersModule],
