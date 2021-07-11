f967bc6b6466863e6e7a1b3c555fe546671b0e00
Teddy <tkhwang@gmail.com>
2021-07-11 10:12:31 +0900

    🛂 (client): Auth/login handling

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,10 +1,24 @@
 import { AppProps } from 'next/app';
+import { makeVar } from '@apollo/client';
 
 import 'tailwindcss/tailwind.css';
 import '@readable/styles/styles.css';
 import '@readable/styles/style.scss';
+import { JWT_TOKEN } from '@readable/common/constants';
+import { useEffect } from 'react';
+
+export const globalToken = makeVar<string>('');
+export const globalIsLoggedIn = makeVar<boolean>(false);
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const token = localStorage.getItem(JWT_TOKEN);
+    console.log('🛂 token = ', token);
+
+    globalToken(token);
+    globalIsLoggedIn(Boolean(token));
+  });
+
   return <Component {...pageProps} />;
 }
 
--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getDataFromTree } from '@apollo/client/react/ssr';
 import withApollo from '@readable/lib/withApollo';
 import DemoPage from '@readable/components/templates/DemoPage';
+import { JWT_TOKEN } from '@readable/common/constants';
+import { globalIsLoggedIn, globalToken } from '@readable/pages/_app';
+import { useRouter } from 'next/router';
 
+/*
 export function Home() {
   return <DemoPage></DemoPage>;
+}
+
+export default withApollo(Home, { getDataFromTree });
+*/
+
+export function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const tokenFromLocalStorage = localStorage.getItem(JWT_TOKEN);
+
+    globalToken(tokenFromLocalStorage);
+    globalIsLoggedIn(Boolean(tokenFromLocalStorage));
+
+    if (!globalIsLoggedIn()) router.push('/login');
+  }, []);
+
+  return <DemoPage />;
 }
 
 export default withApollo(Home, { getDataFromTree });
