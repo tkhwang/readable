f415c3a59a21b356fe8906d08842c91192f019fc
Teddy <tkhwang@gmail.com>
2021-07-11 10:46:14 +0900

    ✏️ (client): Fix typo error

--- apps/client/common/link.ts
+++ apps/client/common/link.ts
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV;
 console.log('🔧 @client : NODE_ENV = ', env);
 
 const serverHostUrl =
-  process.env.NODE_ENV === 'production' ? 'https ://readable-2021.herokuapp.com' : 'http://localhost:7000';
+  process.env.NODE_ENV === 'production' ? 'https://readable-2021.herokuapp.com' : 'http://localhost:7000';
 
 export const serverHost = {
   restUrl: `${serverHostUrl}/rest`,
