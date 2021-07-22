0d9be4fced671cec8841eaa79e097827e0a0172b
Teddy <tkhwang@gmail.com>
2021-07-22 23:34:12 +0900

    ✨ (server): Check URL duplication (#66)

--- apps/server/src/bookmarks/usecases/get-urlinfo/get-urlinfo.usecase.ts
+++ apps/server/src/bookmarks/usecases/get-urlinfo/get-urlinfo.usecase.ts
@@ -1,29 +1,32 @@
 import { Usecase } from '@readable/common/usecase';
 import { GetUrlInfoInput } from './get-urlinfo.input';
-import * as ogs from 'open-graph-scraper';
-import { BookmarkBuilder } from '@readable/bookmarks/infrastructures/typeorm/bookmark.entity.builder';
 import { Bookmark } from '@readable/bookmarks/models/bookmark.model';
+import * as sha256 from 'crypto-js/sha256';
+import { BookmarksService } from '@readable/bookmarks/bookmarks.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Bookmark as BookmarkEntity } from '@readable/bookmarks/infrastructures/typeorm/bookmark.entity';
+import { Repository } from 'typeorm';
 
 export class GetUrlInfoUsecase implements Usecase<GetUrlInfoInput, Bookmark> {
+  constructor(
+    private readonly bookmarksService: BookmarksService,
+    @InjectRepository(BookmarkEntity) private readonly bookmarksRepository: Repository<BookmarkEntity>
+  ) {}
+
   async execute(query: GetUrlInfoInput) {
     const { url } = query;
 
-    const ogsOptions = { url };
-    const { result } = await ogs(ogsOptions);
+    const urlHash = sha256(url).toString();
 
-    const bookmark = new BookmarkBuilder()
-      .setUrl(result['ogUrl'] ?? url)
-      .setSiteName(result['ogSiteName'] ?? '')
-      .setTitle(result['ogTitle'] ?? '')
-      .setType(result['ogType'] ?? '')
-      .setImageUrl(result['ogImage']['url'] ?? '')
-      .setDescription(result['ogDescription'] ?? '')
-      .setTags(result['ogTags'] ?? '')
-      .build();
+    const existingBookmark = await this.bookmarksService.getBookmarkByUrlHash(urlHash);
+    if (existingBookmark) return existingBookmark;
 
-    bookmark.urlHash = '1234';
-    bookmark.generatedImage = '...';
+    const bookmarkInfo = await this.bookmarksService.generateBasicBookmarkInfo(query);
+    bookmarkInfo.urlHash = urlHash;
 
-    return bookmark;
+    // TODO(Teddy): WIP
+    // bookmark.generatedImage = '...';
+
+    return bookmarkInfo;
   }
 }
