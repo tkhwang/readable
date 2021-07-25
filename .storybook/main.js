84573f52dbf0a72bdc167d65e77148926f5ea7ca
zlrlo <easy10612@gmail.com>
2021-07-25 22:40:07 +0900

    ➕ (client): Add a storybook addon-viewport

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
 module.exports = {
-  stories: []
+  stories: [],
+  addons: ['@storybook/addon-viewport/register'],
 };
--- apps/client/.storybook/preview.js
+++ apps/client/.storybook/preview.js
@@ -1,5 +1,12 @@
 import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import 'tailwindcss/tailwind.css';
 
 addDecorator(withKnobs);
+
+export const parameters = {
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+};
