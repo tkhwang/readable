f00a8a3401914cf06c58a3b8e944a2b12c5bbbed
Teddy <tkhwang@gmail.com>
2021-07-29 23:28:51 +0900

    ♻️ (client): Show my bookmarks only

--- apps/client/components/modules/Bookmarks/BookmarksViewController.tsx
+++ apps/client/components/modules/Bookmarks/BookmarksViewController.tsx
@@ -1,6 +1,5 @@
 import { ViewController } from '@readable/types/ViewController';
 import React from 'react';
-import { BookmarkCard } from './BookmarkCard';
 import { BookmarksViewModel } from './useBookmarks.query';
 
 export const BookmarksViewController: ViewController<BookmarksViewModel> = React.memo(({ viewModel }) => {
@@ -14,39 +13,11 @@ export const BookmarksViewController: ViewController<BookmarksViewModel> = React
     );
   }
 
-  // <BookmarkCard title="My Bookmarks" bookmarks={myBookmarks} /> *
-
   return (
     <>
       <p className="text-2xl">My Bookmarks</p>
       {myBookmarks?.length > 0 &&
         myBookmarks.map(bookmark => {
-          return (
-            <a href={bookmark.url}>
-              <div className="p-10">
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                  <img className="w-full" src={bookmark.imageUrl} alt={bookmark.title} />
-                  <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{bookmark.title}</div>
-                    <p className="text-gray-700 text-base">{bookmark.description}</p>
-                  </div>
-                  <div className="px-6 pt-4 pb-2">
-                    {bookmark?.tags?.map(tag => {
-                      return (
-                        <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          {tag}
-                        </span>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </a>
-          );
-        })}
-      <p className="text-2xl">Anonymous Bookmarks</p>
-      {anonymousBookmarks?.length > 0 &&
-        anonymousBookmarks.map(bookmark => {
           return (
             <a href={bookmark.url}>
               <div className="p-10">
