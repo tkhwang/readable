c9db2c8e278cd4f9fa360da9c52f4adc8f17cd13
Teddy <tkhwang@gmail.com>
2021-07-31 10:28:56 +0900

    💄 (client): Add bookmark delete button UI

--- apps/client/components/modules/Bookmarks/BookmarksViewController.tsx
+++ apps/client/components/modules/Bookmarks/BookmarksViewController.tsx
@@ -19,26 +19,29 @@ export const BookmarksViewController: ViewController<BookmarksViewModel> = React
       {myBookmarks?.length > 0 &&
         myBookmarks.map(bookmark => {
           return (
-            <a href={bookmark.url}>
-              <div className="p-10">
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="p-10">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <a href={bookmark.url}>
                   <img className="w-full" src={bookmark.imageUrl} alt={bookmark.title} />
                   <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{bookmark.title}</div>
                     <p className="text-gray-700 text-base">{bookmark.description}</p>
                   </div>
-                  <div className="px-6 pt-4 pb-2">
-                    {bookmark?.tags?.map(tag => {
-                      return (
-                        <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          {tag}
-                        </span>
-                      );
-                    })}
-                  </div>
+                </a>
+                <div className="px-6 pt-4 pb-2">
+                  {bookmark?.tags?.map(tag => {
+                    return (
+                      <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        {tag}
+                      </span>
+                    );
+                  })}
                 </div>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 text-base hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                  delete
+                </button>
               </div>
-            </a>
+            </div>
           );
         })}
     </>
