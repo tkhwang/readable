7bd0dbbc88c88abb36b554f24cafedaec451e2d9
Teddy <tkhwang@gmail.com>
2021-08-01 23:21:37 +0900

    ✨ (server): Pagination Cursor updated

--- apps/server/src/bookmarks/infrastructures/typeorm/repositories/bookmarks.repository.ts
+++ apps/server/src/bookmarks/infrastructures/typeorm/repositories/bookmarks.repository.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PaginationCursor } from '@readable/common/pagination/paginationCursor';
 import { GetPaginationBookmarksInput } from '@readable/pagination/paginationBookmarks/applications/usecases/get-pagination-bookmarks/get-pagination-bookmarks.input';
+import { PaginationWrongCursorExceptoin } from '@readable/pagination/paginationBookmarks/domain/errors/paginationBookmarks.errors';
 import { PaginationBookmarkBRFOs } from '@readable/pagination/paginationBookmarks/domain/models/paginationBookmarks.type';
 import { User } from '@readable/users/domain/models/user.model';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, LessThan, Repository } from 'typeorm';
 import { Bookmark } from '../entities/bookmark.entity';
 
 @Injectable()
@@ -17,16 +18,16 @@ export class BookmarksRepository extends Repository<Bookmark> {
 
     const criteria = {};
 
-    // if (after) {
-    //   const { id: afterId, order: orderInCursor, orderBy: orderByInCursor } = after;
+    if (after) {
+      const { id: afterId, order: orderInCursor, orderBy: orderByInCursor } = after;
 
-    //   // MEMO(Teddy): 요청한 orderBy/order와 커서 안의 정보가 서로 다른 경우 에러를 발생시킨다.
-    //   if (!(order === orderInCursor && orderBy === orderByInCursor)) {
-    //     throw paginationWrongCursor(after, orderBy, order);
-    //   }
+      // MEMO(Teddy): 요청한 orderBy/order와 커서 안의 정보가 서로 다른 경우 에러를 발생시킨다.
+      if (!(order === orderInCursor && orderBy === orderByInCursor)) {
+        throw new PaginationWrongCursorExceptoin(after, orderBy, order);
+      }
 
-    //   criteria.id = { $lt: afterId };
-    // }
+      criteria['id'] = LessThan(afterId);
+    }
 
     let bookmarks = await this.createQueryBuilder('bookmark')
       .where(criteria)
---
+++ apps/server/src/pagination/paginationBookmarks/domain/errors/paginationBookmarks.errors.ts
@@ -0,0 +1,22 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { PaginationCursor, PaginationOrder, PaginationOrderBy } from '@readable/common/pagination/paginationCursor';
+
+export class PaginationWrongCursorExceptoin extends HttpException {
+  constructor(cursor: PaginationCursor, orderBy: PaginationOrderBy, order: PaginationOrder) {
+    const { id, orderBy: orderByInCursor, order: orderInCursor } = cursor;
+
+    const isOrderByMatched = orderBy === orderByInCursor;
+    const isOrderMatched = order === orderInCursor;
+
+    let errorMessage = '';
+
+    if (!isOrderByMatched) {
+      errorMessage += `orderBy (in cursor: ${orderByInCursor}, request: ${orderBy})`;
+    }
+    if (!isOrderMatched) {
+      errorMessage += `order (in cursor: ${orderInCursor}, request: ${order})`;
+    }
+
+    super(errorMessage, HttpStatus.BAD_REQUEST);
+  }
+}
