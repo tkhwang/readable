10616cb5d95f90319307ad5caea657da9991a880
Teddy <tkhwang@gmail.com>
2021-08-01 14:25:35 +0900

    🐛 (server): Fix repository inject error

--- apps/server/src/pagination/paginationBookmarks/applications/usecases/get-pagination-bookmarks/get-pagination-bookmarks.usecase.ts
+++ apps/server/src/pagination/paginationBookmarks/applications/usecases/get-pagination-bookmarks/get-pagination-bookmarks.usecase.ts
@@ -1,3 +1,4 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { BookmarksRepository } from '@readable/bookmarks/infrastructures/typeorm/repositories/bookmarks.repository';
 import { Usecase } from '@readable/common/usecase';
 import { PaginationBookmarks } from '@readable/pagination/paginationBookmarks/domain/paginationBookmarks.type';
@@ -5,7 +6,10 @@ import { User } from '@readable/users/domain/user.model';
 import { GetPaginationBookmarksInput } from './get-pagination-bookmarks.input';
 
 export class GetPaginationBookmarksUsecase implements Usecase<GetPaginationBookmarksInput, PaginationBookmarks | null> {
-  constructor(private readonly bookmarksRepository: BookmarksRepository) {}
+  constructor(
+    @InjectRepository(BookmarksRepository)
+    private readonly bookmarksRepository: BookmarksRepository
+  ) {}
 
   async execute(query: GetPaginationBookmarksInput, requestUser: User) {
     return this.bookmarksRepository.getPaginationBookmarks(query, requestUser);
