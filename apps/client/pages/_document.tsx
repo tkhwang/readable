66792eda952a3ed404572100d8fbbe8721a29269
Teddy <tkhwang@gmail.com>
2021-08-04 22:40:27 +0900

    💥 (common): Apply typescript strict mode

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class CustomDocument extends Document<{ styleTags: ReactElement[] }> {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }: { renderPage: any }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const page = renderPage((App: any) => (props: any) => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
