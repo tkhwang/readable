140f4c61ddd14bca7b670068a8b780ad948fe953
tkhwang <tkhwang@gmail.com>
2021-08-08 23:20:16 +0900

    ✨ (client): Display keyword in bookmark card

--- libs/collection/feature-bookmark/src/lib/BookmarkCard.tsx
+++ libs/collection/feature-bookmark/src/lib/BookmarkCard.tsx
@@ -21,10 +21,10 @@ export const BookmarkCard: FunctionComponent<Props> = ({ title, bookmarks }) => 
                   <p className="text-gray-700 text-base">{bookmark.description}</p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                  {bookmark?.tags?.map(tag => {
+                  {bookmark?.keywords?.map((keyword: string) => {
                     return (
                       <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                        {tag}
+                        {keyword}
                       </span>
                     );
                   })}
