6264a8e6dc00714d5a5363632224585073ec7d09
Teddy <tkhwang@gmail.com>
2021-08-28 15:20:13 +0900

    👷 (common): Test AWS codedeploy script

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,6 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+cd /srv/app
 /usr/bin/npm install
 /usr/bin/npm run server-build
 /usr/bin/npm run start
