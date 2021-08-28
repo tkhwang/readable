da52cc7c913740782af2df613c3d8d0d54c57119
Teddy <tkhwang@gmail.com>
2021-08-28 15:13:56 +0900

    👷 (common): AWS codedeploy path problem fix

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,6 +2,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-npm install
-npm run server-build
-npm run start
+/usr/bin/npm install
+/usr/bin/npm run server-build
+/usr/bin/npm run start
