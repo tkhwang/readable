66d4e6aa0526e69f60ddd97501e42255aeacc851
Teddy <tkhwang@gmail.com>
2021-09-06 23:52:45 +0900

    ✨ (server): Add myInterests Query

--- apps/server/src/interests/interests.module.ts
+++ apps/server/src/interests/interests.module.ts
@@ -4,9 +4,10 @@ import { InterestsRepository } from './infrastructures/mongo/repositories/intere
 import { InterestsService } from './interests.service';
 import { InterestsResolver } from './interests.resolver';
 import { FindOrAddInterestWithAuthUseCase } from './applications/usecases/find-or-add-interest/find-or-add-interest-with-auth.usecase';
+import { GetMyInterestsWithAuthUsecase } from './applications/usecases/get-my-interests-with-auth/get-my-interests-with-auth.usecase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([InterestsRepository])],
-  providers: [InterestsResolver, InterestsService, FindOrAddInterestWithAuthUseCase],
+  providers: [InterestsResolver, InterestsService, GetMyInterestsWithAuthUsecase, FindOrAddInterestWithAuthUseCase],
 })
 export class InterestsModule {}
--- apps/server/src/interests/interests.resolver.ts
+++ apps/server/src/interests/interests.resolver.ts
@@ -1,15 +1,28 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { GqlAuthGuard } from '@readable/auth/domain/graphql-auth.guards';
 import { CurrentUser } from '@readable/middleware/current-user.decorator';
 import { User } from '@readable/users/domain/models/user.model';
 import { FindOrAddInterestWithAuthInput } from './applications/usecases/find-or-add-interest/find-or-add-interest-with-auth.input';
 import { FindOrAddInterestWithAuthUseCase } from './applications/usecases/find-or-add-interest/find-or-add-interest-with-auth.usecase';
+import { GetMyInterestsWithAuthUsecase } from './applications/usecases/get-my-interests-with-auth/get-my-interests-with-auth.usecase';
 import { Interest } from './domain/interest.model';
 
 @Resolver(of => Interest)
 export class InterestsResolver {
-  constructor(private readonly findOrAddInterestWithAuthUseCase: FindOrAddInterestWithAuthUseCase) {}
+  constructor(
+    private readonly findOrAddInterestWithAuthUseCase: FindOrAddInterestWithAuthUseCase,
+    private readonly getMyInterestsWithAuthUsecase: GetMyInterestsWithAuthUsecase
+  ) {}
+
+  /*
+   * Query (as noun)
+   */
+  @Query(returns => [Interest])
+  @UseGuards(GqlAuthGuard)
+  async myInterests(@CurrentUser() requestUser: User) {
+    return this.getMyInterestsWithAuthUsecase.execute(requestUser);
+  }
 
   /*
    * Mutation (as verb)
