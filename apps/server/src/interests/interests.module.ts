61d81ca4df09c64be216f8589930988ff50f07f2
Teddy <tkhwang@gmail.com>
2021-09-07 00:08:28 +0900

    ✨ (server): Add myInterests REST router

--- apps/server/src/interests/interests.module.ts
+++ apps/server/src/interests/interests.module.ts
@@ -5,9 +5,11 @@ import { InterestsService } from './interests.service';
 import { InterestsResolver } from './interests.resolver';
 import { FindOrAddInterestWithAuthUseCase } from './applications/usecases/find-or-add-interest/find-or-add-interest-with-auth.usecase';
 import { GetMyInterestsWithAuthUsecase } from './applications/usecases/get-my-interests-with-auth/get-my-interests-with-auth.usecase';
+import { InterestsController } from './interests.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([InterestsRepository])],
   providers: [InterestsResolver, InterestsService, GetMyInterestsWithAuthUsecase, FindOrAddInterestWithAuthUseCase],
+  controllers: [InterestsController],
 })
 export class InterestsModule {}
