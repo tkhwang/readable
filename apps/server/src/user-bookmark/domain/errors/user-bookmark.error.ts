d0c2334d412382fe33f8a1db32cc58fc438590fd
Teddy <tkhwang@gmail.com>
2021-09-16 13:41:31 +0900

    ✨ (server): Add sync Google calendar usecase

--- apps/server/src/user-bookmark/domain/errors/user-bookmark.error.ts
+++ apps/server/src/user-bookmark/domain/errors/user-bookmark.error.ts
@@ -1,9 +1,18 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+export class UserAuthTokenRefreshTokenNotFoundException extends HttpException {
+  constructor(userId: string, oauthUserId: string) {
+    super(
+      `[-] UserAuthTokenRefreshTokenNotFoundException : User(${userId} OAuthUser(${oauthUserId})'s accessToken and refreshToken are not found.))`,
+      HttpStatus.UNAUTHORIZED
+    );
+  }
+}
+
 export class UnauthorizedDeleteUserBookmarkWithAuthException extends HttpException {
   constructor(userId: string, userBookmarkId: string) {
     super(
-      `[-] DeleteBookmarkWithAuthUsecse : User(${userId} doesn't have the userBookmark(${userBookmarkId}))`,
+      `[-] UnauthorizedDeleteUserBookmarkWithAuthException : User(${userId} doesn't have the userBookmark(${userBookmarkId}))`,
       HttpStatus.UNAUTHORIZED
     );
   }
@@ -12,7 +21,7 @@ export class UnauthorizedDeleteUserBookmarkWithAuthException extends HttpExcepti
 export class DeleteUserBookmarkWithAuthFailException extends HttpException {
   constructor(userId: string, userBookmarkId: string, errorMessage: string) {
     super(
-      `[-] DeleteBookmarkWithAuthUsecse : User(${userId} failed to delete the userBookmark(${userBookmarkId}) due to eror (${errorMessage}))`,
+      `[-] DeleteUserBookmarkWithAuthFailException : User(${userId} failed to delete the userBookmark(${userBookmarkId}) due to eror (${errorMessage}))`,
       HttpStatus.UNAUTHORIZED
     );
   }
