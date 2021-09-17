e1b3a00e29788a50e696f73c9583cec301ebc1f2
Teddy <tkhwang@gmail.com>
2021-09-17 22:23:55 +0900

    ♻️ (libs): Add Interest in card

--- libs/feed/feature-bookmark/src/lib/BookmarksViewController.tsx
+++ libs/feed/feature-bookmark/src/lib/BookmarksViewController.tsx
@@ -28,6 +28,11 @@ export const BookmarksViewController = () => {
                 </div>
               </a>
               <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  {interest.interest || ''}
+                </span>
+              </div>
+              <div className="px-6 pt-4 pb-2">
                 {tags?.map(tag => {
                   return (
                     <span className="inline-block bg-gray-200 rounded-full px-3  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
