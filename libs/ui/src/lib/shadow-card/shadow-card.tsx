5d1a6d18169a8b4e325374f4f38684ba4ee0da8f
zlrlo <easy10612@gmail.com>
2021-09-18 02:16:22 +0900

    💄 (client): shadow-card props 변경

--- libs/ui/src/lib/shadow-card/shadow-card.tsx
+++ libs/ui/src/lib/shadow-card/shadow-card.tsx
@@ -1,20 +1,23 @@
-import Image from 'next/image';
 import CardImage from '../../../assets/card_sample.svg';
 import { BookmarkIcon, DotsVerticalIcon } from '@heroicons/react/solid';
 import { BookmarkIcon as BookmarkOutlineIcon, BadgeCheckIcon } from '@heroicons/react/outline';
-import { Avatar } from '@readable/ui';
-import ProfileImage from '../../../assets/Fernando_Pidrilio.svg';
+import Avatar from '../avatar/avatar';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ShadowCardProps {}
+export interface ShadowCardProps {
+  cardImageUrl?: string;
+  description: string;
+  siteName: string;
+  profileImageUrl?: string;
+  tags?: { id: string; name: string }[];
+}
 
-export function ShadowCard(props: ShadowCardProps) {
+export function ShadowCard({ cardImageUrl, description, siteName, profileImageUrl, tags }: ShadowCardProps) {
   return (
     <div className="shadow-offset-black overflow-hidden">
       {/* Header */}
       <div className="flex p-4 bg-gray-900 justify-between items-center">
         <div className="flex items-center">
-          <Avatar profileImage={ProfileImage} />
+          <Avatar profileImage={profileImageUrl} />
           <div className="text-white ml-2">
             <div className="text-xs">@20min</div>
             <div className="text-xs font-thin">10min ago</div>
@@ -30,28 +33,34 @@ export function ShadowCard(props: ShadowCardProps) {
         </div>
       </div>
       {/* Contents */}
-      <div className="p-4 bg-red-400 opacity-90">
+      <div className="p-4 opacity-90">
         <div className="flex justify-between -mx-2">
           <div className="mx-2 flex-1">
             <div className="text-white font-light text-xs">UX Design</div>
-            <div className="text-white font-bold break-words overflow-hidden line-clamp-3">
-              Announcing Coinbase’s successful transition to React Native test test test
+            <div className="text-white font-bold break-words overflow-hidden line-clamp-3">{description}</div>
+            <div className="flex space-x-2">
+              {tags?.map(({ id, name }) => (
+                <div key={id} className="text-white text-xs mt-2">
+                  #{name}
+                </div>
+              ))}
             </div>
-            <div className="text-white text-xs mt-2">#design</div>
           </div>
-          <div className="flex-1 mx-2 overflow-hidden rounded-sm ">
-            <img src={CardImage} alt="" className="object-cover w-full h-full"></img>
+          <div className="flex-1 mx-2 overflow-hidden rounded-sm w-44 h-44">
+            {/* TODO(zlrlo): default 이미지 작업 필요함 */}
+            <img src={cardImageUrl ?? CardImage} alt="" className="object-cover w-full h-full"></img>
           </div>
         </div>
       </div>
       {/* Footer */}
       <div className="py-3 px-4 bg-red-400 flex flex-wrap justify-between items-center text-white text-xs">
         <div className="flex items-center">
-          <div className="border-2 w-5 h-5" />
-          <div className="ml-3">Siteinspire</div>
+          <div className="border-2 rounded-md w-5 h-5" />
+          <div className="ml-2">{siteName}</div>
         </div>
         <div className="flex items-center space-x-5">
-          <div className="flex items-center">
+          {/* TODO(zlrlo): 북마크 수, 읽은 사람 수 작업 필요 */}
+          {/* <div className="flex items-center">
             <BookmarkOutlineIcon className="w-5 h-5 " />
             <div className="ml-1">2,890 marks</div>
           </div>
@@ -59,7 +68,7 @@ export function ShadowCard(props: ShadowCardProps) {
           <div className="flex items-center">
             <BadgeCheckIcon className="w-5 h-5 " />
             <div className="ml-1">1,594 reads</div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
