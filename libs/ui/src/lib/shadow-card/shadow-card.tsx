00389fa13076975bb168419500d1a6e3eea7f3d9
zlrlo <easy10612@gmail.com>
2021-09-23 20:20:57 +0900

    🐛 (client): 배포 환경에서 bg-color 적용 안되는 문제 해결

--- libs/ui/src/lib/shadow-card/shadow-card.tsx
+++ libs/ui/src/lib/shadow-card/shadow-card.tsx
@@ -19,7 +19,7 @@ export function ShadowCard({ cardImageUrl, description, siteName, profileImageUr
   };
 
   const colors = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
-  const randomColor = colors[index % 9];
+  const bgColor = `bg-${colors[index % 9]}-400`;
 
   return (
     <div className="shadow-offset-black overflow-hidden">
@@ -42,7 +42,7 @@ export function ShadowCard({ cardImageUrl, description, siteName, profileImageUr
         </div>
       </div>
       {/* Contents */}
-      <div className={`p-4 opacity-90 bg-${randomColor}-400`}>
+      <div className={`p-4 opacity-90 ${bgColor}`}>
         <div className="flex -mx-2">
           <div className="mx-2 flex-1">
             <div className="text-white font-light text-xs">UX Design</div>
@@ -72,7 +72,7 @@ export function ShadowCard({ cardImageUrl, description, siteName, profileImageUr
         </div>
       </div>
       {/* Footer */}
-      <div className={`py-3 px-4 bg-${randomColor}-400 flex flex-wrap justify-between items-center text-white text-xs`}>
+      <div className={`py-3 px-4 ${bgColor} flex flex-wrap justify-between items-center text-white text-xs`}>
         <div className="flex items-center">
           <div className="border-2 rounded-md w-5 h-5" />
           <div className="ml-2">{siteName}</div>
