1e56b678542bb5542f32503476d52d0da40c64f8
Teddy <tkhwang@gmail.com>
2021-10-04 14:26:25 +0900

    ♻️ (server): Change FindPopularTags from get to find

--- apps/server/src/tags/applications/usercases/find-popular-tags-with-auth/find-popular-tags-with-auth.input.ts
+++ apps/server/src/tags/applications/usercases/find-popular-tags-with-auth/find-popular-tags-with-auth.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class GetPopularTagsWithAuthInput {
+export class FindPopularTagsWithAuthInput {
   @Field(type => Number)
   howMany: number;
 }
--- apps/server/src/tags/applications/usercases/find-popular-tags-with-auth/find-popular-tags-with-auth.usecase.ts
+++ apps/server/src/tags/applications/usercases/find-popular-tags-with-auth/find-popular-tags-with-auth.usecase.ts
@@ -3,13 +3,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Usecase } from '@readable/common/applications/usecase';
 import { TagsRepository } from '@readable/tags/infrastructures/typeorm/repositories/tags.repository';
 import { User } from '@readable/users/domain/models/user.model';
-import { GetPopularTagsWithAuthInput } from './get-popular-tags-with-auth.input';
+import { FindPopularTagsWithAuthInput } from './find-popular-tags-with-auth.input';
 
 @Injectable()
-export class GetPopularTagsWithAuthUsecase implements Usecase<GetPopularTagsWithAuthInput, any> {
+export class FindPopularTagsWithAuthUsecase implements Usecase<FindPopularTagsWithAuthInput, any> {
   constructor(@InjectRepository(TagsRepository) private readonly tagsRepository: TagsRepository) {}
 
-  async execute(query: GetPopularTagsWithAuthInput, requestUser: User) {
+  async execute(query: FindPopularTagsWithAuthInput, requestUser: User) {
     const { howMany } = query;
 
     return this.tagsRepository.getPopularTags(howMany);
