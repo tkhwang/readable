dc1cf64cae1213180e3aaeb67413751a98393c46
zlrlo <easy10612@gmail.com>
2021-10-06 21:33:00 +0900

    ♻️ (client): search 코드 리팩토링
    - home-search-dropdown-menu 컴포넌트 추가

--- libs/home/feature-home/src/lib/home-header.tsx
+++ libs/home/feature-home/src/lib/home-header.tsx
@@ -1,6 +1,13 @@
-import { MainHeader } from '@readable/ui';
+import { MainHeader, SearchDropdownMenuProps } from '@readable/ui';
 import { HomeProfile } from './home-profile';
+import { SearchDropdownMenu } from './home-search-dropdown-menu';
 
 export const HomeHeader = () => {
-  return <MainHeader renderProfile={() => <HomeProfile />} />;
+  return (
+    <MainHeader renderProfileDropdown={() => <HomeProfile />}>
+      {({ onClose }: SearchDropdownMenuProps) => {
+        return <SearchDropdownMenu onClose={onClose} />;
+      }}
+    </MainHeader>
+  );
 };
--- libs/ui/src/lib/form/form-with-underline.tsx
+++ libs/ui/src/lib/form/form-with-underline.tsx
@@ -1,30 +1,30 @@
 import { SearchIcon } from '@heroicons/react/solid';
-import { useRef } from 'react';
+import { FormEvent, useRef } from 'react';
 
 export type FormWithUnderlineProps = {
   inputValue?: string;
-  handleSubmit?: (inputValue: string) => void;
+  onSubmit: (inputValue: string) => void;
 };
 
-export function FormWithUnderline({ inputValue, handleSubmit }: FormWithUnderlineProps) {
+export function FormWithUnderline({ inputValue, onSubmit }: FormWithUnderlineProps) {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const onSubmit = (e: any) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (inputRef.current) {
-      handleSubmit && handleSubmit(inputRef.current.value ?? '');
+      onSubmit(inputRef.current.value ?? '');
     }
   };
 
   return (
-    <form onSubmit={onSubmit} className="w-full">
+    <form onSubmit={handleSubmit} className="w-full">
       <div className="flex items-center border-b border-gray-500">
         <button className="flex-shrink-0  py-1 px-2" type="button">
           <SearchIcon className="w-10 h-10 text-gray-500" />
         </button>
         <input
           ref={inputRef}
-          className="appearance-none bg-transparent border-none w-full  mr-3 py-1 px-2 leading-tight focus:outline-none text-3xl font-bold"
+          className="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none text-3xl font-bold"
           type="text"
           placeholder="Search"
           aria-label="Search Keyword"
