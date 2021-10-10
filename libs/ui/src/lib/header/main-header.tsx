b688fb0b5f20862ec70359bd92a608e860cd407d
zlrlo <easy10612@gmail.com>
2021-10-11 01:30:03 +0900

    💄 (client): home page layout 수정
    - 헤더 가운데 정렬
    - 헤더와 main 영역 사이 간격 수정

--- libs/ui/src/lib/header/main-header.tsx
+++ libs/ui/src/lib/header/main-header.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { SearchIcon } from '@heroicons/react/solid';
-import Link from 'next/link';
+import { BookmarkIcon } from '@heroicons/react/outline';
 import { useState } from 'react';
 import Logo from '../logo/logo';
+import { TabsWithUnderline } from '../navigation/tabs-with-underline';
 
 export interface SearchDropdownMenuProps {
   onClose: () => void;
@@ -25,63 +26,43 @@ export function MainHeader({ renderProfileDropdown, children }: MainHeaderProps)
     setSearchDropdown(!searchDropdown);
   };
 
+  const renderButtonGroup = () => {
+    return (
+      <>
+        <button onClick={() => toggleSearchDropdown()} className="hover:opacity-30">
+          {children && <SearchIcon className="w-7 h-7" />}
+        </button>
+        <button className="hover:opacity-30">
+          <BookmarkIcon className="w-7 h-7" />
+        </button>
+        <div>{renderProfileDropdown && <>{renderProfileDropdown()}</>}</div>
+      </>
+    );
+  };
+
   return (
     <>
-      <div className="flex items-center justify-between flex-wrap p-6">
-        {/* Logo */}
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <div className="mr-2">
-            <Logo />
+      <div className="flex items-center justify-between flex-wrap">
+        <button className="flex items-center flex-shrink-0 mr-6">
+          <Logo />
+        </button>
+
+        <div className="flex items-center lg:hidden space-x-5">{renderButtonGroup()}</div>
+
+        <nav className={`w-full flex-grow lg:flex lg:items-center lg:w-auto`}>
+          <div className="text-sm lg:flex-grow lg:mt-0 mt-4">
+            <TabsWithUnderline
+              tabs={[
+                { id: '1', name: 'Following' },
+                { id: '2', name: 'Recommend' },
+              ]}
+            />
           </div>
-          <span className="font-semibold text-xl tracking-tight">Readable</span>
-        </div>
 
-        {/* Menu Button */}
-        <div className="flex lg:hidden">
-          <button
-            onClick={() => toggleMenuDropdown()}
-            className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
-          >
-            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-
-        <div className={`w-full flex-grow ${menuDropdown ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto`}>
-          {/* Navigation */}
-          <nav className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            >
-              Explore
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            >
-              My Page
-            </a>
-            <Link href="/mvpSearch">
-              <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">Search (MVP)</a>
-            </Link>
-            <Link href="/mvpTag">
-              <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">Tag (MVP)</a>
-            </Link>
-          </nav>
-
-          {/* Dropdown */}
-          {children && (
-            <div className="mr-8 hidden lg:block">
-              <SearchIcon onClick={() => toggleSearchDropdown()} className="w-7 h-7 text-white" />
-            </div>
-          )}
-          {renderProfileDropdown  && <div className="hidden lg:block">{renderProfileDropdown()}</div>}
-        </div>
+          <div className="hidden lg:flex items-center space-x-7">{renderButtonGroup()}</div>
+        </nav>
       </div>
-      {children && searchDropdown && children({onClose: toggleSearchDropdown})}
+      {children && searchDropdown && children({ onClose: toggleSearchDropdown })}
     </>
   );
 }
--- libs/ui/src/lib/layout/two-columns-layout.tsx
+++ libs/ui/src/lib/layout/two-columns-layout.tsx
@@ -12,13 +12,17 @@ export function TwoColumnsLayout({
   renderFooter,
 }: TwoColumnsLayoutProps) {
   return (
-    <div className="max-w-7xl ml-auto mr-auto">
-      <header className="pt-11 sticky top-0 z-10">{renderHeader()}</header>
-      <main className="pt-7 grid grid-cols-12 gap-7 min-h-screen">
-        <section className="sm:col-span-8 col-span-12">{renderFirstColumn()}</section>
-        <section className="col-span-4">{renderSecondColumn()}</section>
-      </main>
+    <>
+      <div className="sticky top-0 z-10 pt-7 pb-5 bg-customGray">
+        <header className="max-w-7xl ml-auto mr-auto">{renderHeader()}</header>
+      </div>
+      <div className="box-content px-6 max-w-7xl ml-auto mr-auto">
+        <main className="grid grid-cols-12 gap-7 min-h-screen mt-4">
+          <section className="sm:col-span-8 col-span-12">{renderFirstColumn()}</section>
+          <section className="col-span-4 sticky top-11">{renderSecondColumn()}</section>
+        </main>
+      </div>
       <footer>{renderFooter()}</footer>
-    </div>
+    </>
   );
 }
