28878d0f011a7b7c904e1b87f29fa2e7fd310beb
zlrlo <easy10612@gmail.com>
2021-10-19 00:31:52 +0900

    💄 (client): 마이페이지 profile 영역 UI 컴포넌트 작업 중

--- libs/collection/feature-collection/src/lib/collection-profile.tsx
+++ libs/collection/feature-collection/src/lib/collection-profile.tsx
@@ -1,5 +1,6 @@
+import { DEFAULT_CARD_COVER_IMAGE_URL } from '@readable/shared/util-common';
 import { Profile } from '@readable/ui';
 
 export const CollectionProfile = () => {
-  return <Profile backgroundImageUrl={'/images/pattern/dot.svg'} />;
+  return <Profile backgroundImageUrl={'/images/pattern/dot.svg'} profileImageUrl={DEFAULT_CARD_COVER_IMAGE_URL} />;
 };
