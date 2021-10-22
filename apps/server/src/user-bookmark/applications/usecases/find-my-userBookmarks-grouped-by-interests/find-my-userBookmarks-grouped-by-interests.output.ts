40df6ca23a6225c72bd63b7ce865c13f987600a3
Teddy <tkhwang@gmail.com>
2021-10-22 18:52:09 +0900

    ♻️ (server): Change output format of myUserBookmarksGroupedByInterests (#177)

--- apps/server/src/user-bookmark/applications/usecases/find-my-userBookmarks-grouped-by-interests/find-my-userBookmarks-grouped-by-interests.output.ts
+++ apps/server/src/user-bookmark/applications/usecases/find-my-userBookmarks-grouped-by-interests/find-my-userBookmarks-grouped-by-interests.output.ts
@@ -3,14 +3,28 @@ import { UserBookmark as UserBookmarkModel } from '@readable/user-bookmark/domai
 import { UserBookmark as UserBookmarkEntity } from '@readable/user-bookmark/infrastructures/typeorm/entities/user-bookmark.entity';
 
 @ObjectType()
-export class FindMyUserBookmarksGroupedByInterestsOutput {
+export class FindMyUserBookmarksGroupedByInterestData {
+  @Field(type => String)
+  interestId: string;
+
   @Field(type => String)
   interest: string;
+
+  constructor(interestId: string, interest: string) {
+    this.interestId = interestId;
+    this.interest = interest;
+  }
+}
+
+@ObjectType()
+export class FindMyUserBookmarksGroupedByInterestsOutput {
+  @Field(type => FindMyUserBookmarksGroupedByInterestData)
+  interest: FindMyUserBookmarksGroupedByInterestData;
 
   @Field(type => [UserBookmarkModel])
   userBookmarks: UserBookmarkEntity[];
 
-  constructor(interest: string, userBookmarks: UserBookmarkEntity[]) {
+  constructor(interest: FindMyUserBookmarksGroupedByInterestData, userBookmarks: UserBookmarkEntity[]) {
     this.interest = interest;
     this.userBookmarks = userBookmarks;
   }
