4e436075169b05851e3f89cb1689104ed25c2b41
zlrlo <easy10612@gmail.com>
2021-10-30 23:06:30 +0900

    ✨ (client): public badge image 추가

--- libs/shared/util-common/src/lib/assets_url.ts
+++ libs/shared/util-common/src/lib/assets_url.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_PROFILE_IMAGE_URL = '/images/avatar/avatar_default.svg';
 export const DEFAULT_CARD_COVER_IMAGE_URL = '/images/card/card_cover_default.svg';
 export const DEFAULT_FAVICON_IMAGE_URL = '/images/favicon.png';
+export const DEFAULT_BADGE_1K_IMAGE_URL = '/images/badge_1k';
+export const DEFAULT_BADGE_HEART_IMAGE_URL = '/images/badge_heart';
