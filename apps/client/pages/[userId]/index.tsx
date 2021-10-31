8ac13f9d56dbdaf2ff9fda17e2032a828851fc51
zlrlo <easy10612@gmail.com>
2021-10-31 21:39:56 +0900

    ✨ (client): my page 최상단 login 처리

--- apps/client/pages/[userId]/index.tsx
+++ apps/client/pages/[userId]/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { FeatureCollection } from '@readable/collection/feature-collection';
+import { useMe } from '@readable/shared/data-access-me';
+import { Loading } from '@readable/ui';
 
 function Home() {
   const router = useRouter();
   const { userId } = router.query;
-  console.log('TCL: Home -> userId', userId);
 
-  return <FeatureCollection />;
+  const { me, isMeDataLoading } = useMe({ redirectTo: '/login', fetchPolicy: 'network-only' });
+
+  if (!me || isMeDataLoading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
+
+  return <FeatureCollection userId={userId as string} />;
 }
 
 export default Home;
