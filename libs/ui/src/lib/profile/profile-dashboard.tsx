89cc5409e3b68e28d21d6e841c087e33b8744cde
zlrlo <easy10612@gmail.com>
2021-10-31 21:17:48 +0900

    💄 (client): my page profile 반응형 UI 적용

--- libs/ui/src/lib/profile/profile-dashboard.tsx
+++ libs/ui/src/lib/profile/profile-dashboard.tsx
@@ -1,3 +1,4 @@
+import { BookmarkIcon } from '@heroicons/react/solid';
 import { Badge, ProfileBadge } from './profile-badge';
 
 export interface ProfileDashboardProps {
@@ -9,16 +10,27 @@ export function ProfileDashboard({ badges }: ProfileDashboardProps) {
     <>
       <div className="md:hidden block py-4 w-3/5">
         <div className="py-4 px-3 leading-relaxed border-2 bg-indigo-700 border-black border-dashed shadow-offset-black text-xs">
-          <div>I have 3 Interests</div>
-          <div>I have 3 Interests</div>
-          <div>I have 3 Interests</div>
+          <div className="truncate">I have 3 Interests</div>
+          <div className="truncate">I got 2,890 bookmarks </div>
+          <div className="truncate">I read 1,598 readable</div>
         </div>
       </div>
       <div className="md:hidden flex justify-end space-x-1 w-2/5 ml-7">
         <ProfileBadge badges={badges} />
       </div>
 
-      <div className="md:block hidden max-w-xs w-full ml-auto h-4/6 border-2 border-black border-dashed bg-indigo-700 shadow-offset-black"></div>
+      <div className="space-y-2 md:flex hidden flex-col p-7 justify-end text-right max-w-xs w-full ml-auto h-4/6 border-2 border-black border-dashed bg-indigo-700 shadow-offset-black">
+        <BookmarkIcon className="w-5 h-5 ml-auto" />
+        <div className="truncate">
+          <span className="lg:inline hidden">I have </span>3 Interests
+        </div>
+        <div className="truncate">
+          <span className="lg:inline hidden">I got </span>2,890 bookmarks{' '}
+        </div>
+        <div className="truncate">
+          <span className="lg:inline hidden">I read </span>1,598 readable
+        </div>
+      </div>
     </>
   );
 }
