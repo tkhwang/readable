be61f1150047ff1c8eec3e9db3e8416e934199a9
zlrlo <easy10612@gmail.com>
2021-11-17 01:57:19 +0900

    🚚 (client): coming-soon -> onboarding
    - feature coming soom을 feature-onboarding으로 변경

--- apps/client/pages/welcome.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FeatureComingSoon } from '@readable/home/feature-coming-soon';
-
-function Index() {
-  return <FeatureComingSoon />;
-}
-
-export default Index;
